d6827a1e9e964eb1aa985da3b4ae2ab4b952a961
Dominik-Topic <dominik.topic@fer.hr>
2025-11-14 19:12:18 +0100

    Update AdminController.java

--- backend/src/main/java/com/pcelice/backend/controller/AdminController.java
+++ backend/src/main/java/com/pcelice/backend/controller/AdminController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:8081"}, allowCredentials = "true")
 public class AdminController {
 
     @Autowired
