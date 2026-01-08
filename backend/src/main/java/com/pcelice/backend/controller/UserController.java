081a37089189a1f6c0f87fa6ad8360ff285f4f82
Bruno Pleše <bruno.plese@fer.unizg.hr>
2026-01-08 15:28:08 +0100

    debug

--- backend/src/main/java/com/pcelice/backend/controller/UserController.java
+++ backend/src/main/java/com/pcelice/backend/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pcelice.backend.controller;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,10 @@ import java.util.Map;
 public class UserController {
 
     @GetMapping
-    public Map<String, Object> getCurrentUser(@AuthenticationPrincipal OAuth2User principal) {
-        if (principal == null) {
+    public Map<String, Object> getCurrentUser(Authentication authentication) {
+        if (authentication == null) {
             return Collections.emptyMap();
         }
-        return Collections.singletonMap("name", principal.getAttribute("name"));
+        return Collections.singletonMap("name", authentication.getName());
     }
 }
