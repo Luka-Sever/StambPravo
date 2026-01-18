7be6a632bcc945095c5e49921a4d58fcf74c6c32
Bruno Pleše <bruno.plese@fer.unizg.hr>
2026-01-18 14:28:31 +0100

    add item dodan

--- backend/src/main/java/com/pcelice/backend/controller/MeetingController.java
+++ backend/src/main/java/com/pcelice/backend/controller/MeetingController.java
@@ -1,6 +1,9 @@
 package com.pcelice.backend.controller;
 
+import com.pcelice.backend.entities.Item;
 import com.pcelice.backend.entities.Meeting;
+import com.pcelice.backend.entities.MeetingItemId;
+import com.pcelice.backend.repositories.MeetingRepository;
 import com.pcelice.backend.service.MeetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,8 @@ public class MeetingController {
 
     @Autowired
     private MeetingService meetingService;
+    @Autowired
+    private MeetingRepository meetingRepository;
 
     @GetMapping
     public List<Meeting> getAllMeetings() {
@@ -23,5 +28,26 @@ public class MeetingController {
     @PostMapping("/newMeeting")
     public ResponseEntity<Meeting> createMeeting(@RequestBody Meeting meeting) {
         return ResponseEntity.ok(meetingService.createMeeting(meeting));
+    }
+
+    @PostMapping("/{meetingId}/items")
+    public ResponseEntity<Meeting> addItem(
+            @PathVariable Integer meetingId,
+            @RequestBody Item item)
+    {
+        Meeting meeting = meetingRepository.findByMeetingId(meetingId).orElse(null);
+
+        if (meeting != null) {
+            MeetingItemId meetingItemId = new MeetingItemId();
+            meetingItemId.setMeetingId(meetingId);
+            meetingItemId.setItemNumber(meeting.getItems().getLast().getItemNumber() + 1);
+            item.setMeeting(meeting);
+            item.setId(meetingItemId);
+            meeting.getItems().add(item);
+            return ResponseEntity.ok(meeting);
+        }
+        else  {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
