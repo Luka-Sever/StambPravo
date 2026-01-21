13eb7e66af85fc8d3626964574fc6b47ea4d2443
DoVu4 <dv56213@fer.hr>
2026-01-21 20:58:29 +0100

    popravljen login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -34,7 +34,7 @@ function Login() {
             <div className="auth-field">
               <label>EMAIL ILI KORISNIČKO IME</label>
               <input 
-                type="email"
+                type="textl"
                 value={loginToken}
                 onChange={(e) => setLoginToken(e.target.value)}
                 placeholder="Unesite email ili korisničko ime"
