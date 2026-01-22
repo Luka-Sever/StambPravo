f0927ff919dc244dc65aa49c29a90cd4bf96c509
Engineer_DUDE <luka.sever20@gmail.com>
2026-01-22 13:37:41 +0100

    Dodan cache sa brže podizanje diskusije

--- backend/src/main/java/com/pcelice/backend/BackendApplication.java
+++ backend/src/main/java/com/pcelice/backend/BackendApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @RestController
+@EnableCaching
 public class BackendApplication {
     @Autowired
     private CoOwnerRepository coOwnerRepository;
