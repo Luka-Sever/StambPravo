f16f5d140eb5ad391b0f31c1d9617bce9f29c03d
Dominik Topić <dominik.topic@fer.unizg.hr>
2026-01-23 22:41:36 +0100

    Onemogućeno više repova u zgradi

--- backend/src/main/java/com/pcelice/backend/service/implementation/BuildingServiceJpa.java
+++ backend/src/main/java/com/pcelice/backend/service/implementation/BuildingServiceJpa.java
@@ -3,12 +3,14 @@ package com.pcelice.backend.service.implementation;
 import com.pcelice.backend.dto.addRepData;
 import com.pcelice.backend.entities.Building;
 import com.pcelice.backend.entities.CoOwner;
+import com.pcelice.backend.entities.RoleType;
 import com.pcelice.backend.repositories.BuildingRepository;
 import com.pcelice.backend.repositories.CoOwnerRepository;
 import com.pcelice.backend.service.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
 
 @Service
 public class BuildingServiceJpa implements BuildingService {
@@ -34,9 +36,23 @@ public class BuildingServiceJpa implements BuildingService {
     }
 
     @Override
+    @Transactional
     public void addRep(addRepData addRepData) {
         CoOwner rep = coOwnerRepository.findByEmail(addRepData.getRepEmail()).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
         Building building = buildingRepository.findByBuildingId(addRepData.getBuildingId()).orElseThrow(() -> new UsernameNotFoundException("Building not found"));
+
+        if (building.getRep()) {
+            CoOwner currentRep = building.getRep();
+            currentRep.setRole(RoleType.CO_OWNER);
+        }
+
+        if (rep.getRole() != RoleType.REP) {
+            rep.setRole(RoleType.REP);
+        }
+
+        if (rep.getBuilding() != building) {
+            throw new RuntimeException("User isnt member of target building");
+        }
 
         building.setRep(rep);
 
